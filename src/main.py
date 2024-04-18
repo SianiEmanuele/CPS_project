991f6ee83eaf76eab189a154c988f861c2e92c4e
SianiEmanuele <sianiemanuele@gmail.com>
2024-04-18 22:46:06 +0200

    - Added requirements.txt -Added plots - Increased number of runs for ISTA

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import os as os
 import numpy as np
+import matplotlib.pyplot as plt
 from utils.algorithms import *
         
-def ISTA_20_runs( p, q, C, tau, lam, x_sparsity):
+def ISTA( runs, p, q, C, tau, lam, x_sparsity):
     #parameters definition with suggested settings
     q=q
     p=p
@@ -13,7 +14,7 @@ def ISTA_20_runs( p, q, C, tau, lam, x_sparsity):
     num_iterations = []
 
     #running the simulation 20 times
-    while(i < 20):        
+    for _ in range(runs):        
         # generating x_tilda with 2-sparsity
         x_tilda = np.zeros(p)
         x_tilda[np.random.choice(p, x_sparsity, replace=False)] = np.random.choice([-1, 1], 2) * np.random.uniform(1, 2)
@@ -26,72 +27,31 @@ def ISTA_20_runs( p, q, C, tau, lam, x_sparsity):
         x_estimated_supports.append(x_estimated_supp)
         num_iterations.append(iterations)
 
-        i+=1
     correct_estimations = 0
-    for j in range(20):
+    for j in range(runs):
         if np.array_equal(x_tilda_supports[j], x_estimated_supports[j]):
             correct_estimations += 1
     return correct_estimations, num_iterations
 
-# def ISTA_20_runs_with_attacks( p, q, C, tau, lam, eta, x_sparsity, a_sparsity, attack_type):
-#     #parameters definition with suggested settings
-#     q=q
-#     p=p
-#     i=0 #counter
 
-#     x_tilda_supports = []
-#     x_estimated_supports = []
-#     a_supports = []
-#     num_iterations = []
-#     estimation_accuracy = []
-
-#     #running the simulation 20 times
-#     while(i < 20):        
-#         # generating x_tilda with standard normal distribution
-#         x_tilda = np.random.rand(p)
-
-#         x_tilda_supports.append (np.where(x_tilda != 0)[0])
-        
-#         a = np.zeros(q)
-#         a[np.random.choice(p, a_sparsity, replace=False)] = np.random.choice([-1, 1], 2) * np.random.uniform(1, 2)
-#         print(a)
-#         a_supports.append (np.where(a != 0)[0])
-
-#         eta = eta
-
-#         if(attack_type == "UNAWARE"):
-#             y = np.dot(C, x_tilda) + eta + a
-#         elif (attack_type == "AWARE"):
-#             y = np.dot(C, x_tilda) + eta
-#             for i in a_supports[-1]:
-#                 y[i] = y[i] / 2
-#         x_estimated, x_estimated_supp, iterations = ISTA(np.zeros(p),C,tau,lam,y)
-#         x_estimated_supports.append(x_estimated_supp)
-#         num_iterations.append(iterations)
-
-#         i+=1
-#     correct_estimations = 0
-
-#     for j in range(20):
-#         if np.array_equal(x_tilda_supports[j], x_estimated_supports[j]):
-#             correct_estimations += 1
-#     estimation_accuracy.append((np.linalg.norm(x_tilda - x_estimated, ord=2)**2))
-#     return correct_estimations, num_iterations, np.mean(estimation_accuracy)
-
-def ISTA_20_runs_with_attacks(n, q, C, tau, lam, eta, x_sparsity, a_sparsity, attack_type):
+def ISTA_with_attacks(n, q, C, tau, lam, x_sparsity, a_sparsity, attack_type, noisy):
     correct_estimations = 0
     num_iterations = []
     estimation_accuracy = []
 
     for _ in range(20):
-        # Generate xe with standard normal distribution
+        # Generate x_tilda with standard normal distribution
         x_tilda = np.random.randn(n)
 
         # Generate the attack vector a
         a = np.zeros(q)
         attack_indices = np.random.choice(q, a_sparsity, replace=False)
         a[attack_indices] = np.random.choice([-2, -1, 1, 2], a_sparsity)
-        #print("vector a:", a)
+
+        if(noisy):
+            eta = 10**(-2) * np.random.randn(q)
+        else:
+            eta = np.zeros(q)
 
         # Generate the measurements y
         if attack_type == "UNAWARE":
@@ -127,8 +87,8 @@ def ISTA_20_runs_with_attacks(n, q, C, tau, lam, eta, x_sparsity, a_sparsity, at
 
     return attack_detection_rate, num_iterations, mean_estimation_accuracy
 
-#Exercise 1
-def exercise_1():
+#task 1
+def task_1():
 
     q=10
     p=20
@@ -138,45 +98,68 @@ def exercise_1():
     lam = 1 / (100*tau)
     sparsity = 2
 
+    runs = 1000
+
     print("\nFIRST EXERCISE WITH SUGGESTED PARAMETERS (q=10, p=20)\n")
 
     print("             QUESTION 1\n- Support recovery rate: how many times the support of x_tilda is correctly estimated?")
-    correct_estimations, num_iterations = ISTA_20_runs(p, q, C, tau, lam, sparsity)
-    print("The support of x_tilda is correctly estimated in ", correct_estimations, " out of 20 runs" , 
+    correct_estimations, num_iterations = ISTA(runs, p, q, C, tau, lam, sparsity)
+    print("The support of x_tilda is correctly estimated in ", correct_estimations, " out of", runs ," runs" , 
           "\nMin iterations = ", min(num_iterations), " || Max iterations = ", max(num_iterations), " || Mean Iterations = ", np.mean(num_iterations), "\n")
+    
+    # # #parameters calculation with q=15
+    # q = 20
+    # C = np.random.rand(q, p)
+    # C_l_2_norm = np.linalg.norm(C, ord=2)
+    # tau = 1 / (C_l_2_norm**2) - 10**(-8)
+    # lam = 1 / (100*tau)
+    
+    # #simulation 
+    # correct_estimations, num_iterations = ISTA(runs, p, q, C, tau, lam, sparsity)
+    # print("q = ", q, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of ", runs, " runs" , "\n")
+
+    # #parameters calculation with q=20
+    # q = 30
+    # C = np.random.rand(q, p)
+    # C_l_2_norm = np.linalg.norm(C, ord=2)
+    # tau = 1 / (C_l_2_norm**2) - 10**(-8)
+    # lam = 1 / (100*tau)
+
+    # #simulation 
+    # correct_estimations, num_iterations = ISTA(runs, p, q, C, tau, lam, sparsity)
+    # print("q = ", q, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of ", runs, " runs" , "\n")
+    # #parameters calculation with q=25
+    # q = 100
+    # C = np.random.rand(q, p)
+    # C_l_2_norm = np.linalg.norm(C, ord=2)
+    # tau = 1 / (C_l_2_norm**2) - 10**(-8)
+    # lam = 1 / (100*tau)
+
+    # GRAPHS
+    q_list = range(10, 51)
+    correct_estimations_percentage = []
+
     print("             QUESTION 2\n- Can we obtain 100% of success in the support recovery by increasing q?\n")
-    
-    #parameters calculation with q=15
-    q = 20
-    C = np.random.rand(q, p)
-    C_l_2_norm = np.linalg.norm(C, ord=2)
-    tau = 1 / (C_l_2_norm**2) - 10**(-8)
-    lam = 1 / (100*tau)
-    
-    #simulation 
-    correct_estimations, num_iterations = ISTA_20_runs(p, q, C, tau, lam, sparsity)
-    print("q = ", q, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of 20 runs" , "\n")
+    for q in q_list:
+        C = np.random.rand(q, p)
+        C_l_2_norm = np.linalg.norm(C, ord=2)
+        tau = 1 / (C_l_2_norm**2) - 10**(-8)
+        lam = 1 / (100*tau)
+        correct_estimations, num_iterations = ISTA(runs, p, q, C, tau, lam, sparsity)
+        correct_estimations_percentage.append(correct_estimations*100/runs)
+        print("q = ", q, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of ", runs, " runs" , "\n")
+    #plotting the results
+    plt.plot(q_list, correct_estimations_percentage)
+    plt.xlabel("q")
+    plt.ylabel("Correct estimations percentage")
+    plt.title("Percentage of correct estimations in function of q")
+    plt.show()
 
-    #parameters calculation with q=20
-    q = 30
-    C = np.random.rand(q, p)
-    C_l_2_norm = np.linalg.norm(C, ord=2)
-    tau = 1 / (C_l_2_norm**2) - 10**(-8)
-    lam = 1 / (100*tau)
 
-    #simulation 
-    correct_estimations, num_iterations = ISTA_20_runs(p, q, C, tau, lam, sparsity)
-    print("q = ", q, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of 20 runs" , "\n")
-    #parameters calculation with q=25
-    q = 100
-    C = np.random.rand(q, p)
-    C_l_2_norm = np.linalg.norm(C, ord=2)
-    tau = 1 / (C_l_2_norm**2) - 10**(-8)
-    lam = 1 / (100*tau)
 
     #simulation
-    correct_estimations, num_iterations = ISTA_20_runs(p, q, C, tau, lam, sparsity)
-    print("q = ", q, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of 20 runs" , "\n")
+    correct_estimations, num_iterations = ISTA(runs, p, q, C, tau, lam, sparsity)
+    print("q = ", q, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of ", runs, " runs" , "\n")
 
     ##################################### QUESTION 4 ##############################################################
 
@@ -193,16 +176,16 @@ def exercise_1():
     lam = gamma / tau
 
     #simulation 
-    correct_estimations, num_iterations = ISTA_20_runs(p, q, C, tau, lam, sparsity)
-    print("tau = ", tau, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of 20 runs" , "\n")
+    correct_estimations, num_iterations = ISTA(runs, p, q, C, tau, lam, sparsity)
+    print("tau = ", tau, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of ", runs, " runs" , "\n")
     
     #different tau
     tau = 1 / (C_l_2_norm**2) - 10**(-12)
     lam = gamma / tau
 
     #simulation
-    correct_estimations, num_iterations = ISTA_20_runs(p, q, C, tau, lam, sparsity)
-    print("tau = ", tau, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of 20 runs" , "\n")
+    correct_estimations, num_iterations = ISTA(runs, p, q, C, tau, lam, sparsity)
+    print("tau = ", tau, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of ", runs, " runs" , "\n")
 
     ##################################### QUESTION 5 ##############################################################
     print("             QUESTION 5\nTry different values for λ , by keeping τ constant\n")
@@ -214,38 +197,45 @@ def exercise_1():
     lam = 1 / (1000*tau)
     
     #simulation
-    correct_estimations, num_iterations = ISTA_20_runs(p, q, C, tau, lam, sparsity)
-    print("lam = ", lam, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of 20 runs" , "\n")
+    correct_estimations, num_iterations = ISTA(runs, p, q, C, tau, lam, sparsity)
+    print("lam = ", lam, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of ", runs, " runs" , "\n")
 
     #bigger lambda
     lam = 1 / (10*tau)
 
     #simulation
-    correct_estimations, num_iterations = ISTA_20_runs(p, q, C, tau, lam, sparsity)
-    print("lam = ", lam, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of 20 runs" , "\n")
+    correct_estimations, num_iterations = ISTA(runs, p, q, C, tau, lam, sparsity)
+    print("lam = ", lam, "|| The support of x_tilda is correctly estimated in ", correct_estimations, " out of ", runs, " runs" , "\n")
 
 
     
-# exercise_1()
 
-def exercise_2():
+# task_2
+def task_2():
     q=20
     p=10
-    sparsity = p
+    x_sparsity = p
+    a_sparsity = 2
     C = np.random.rand(q, p)
     #calculate tau as a vector with q zeroes and p ones
     C_l_2_norm = np.linalg.norm(C, ord=2)
     tau = 1 / (C_l_2_norm**2) - 10**(-8)
     lam = 2 * 10**(-3) / tau
 
-    print(lam)
+    print("\nSECOND EXERCISE WITH SUGGESTED PARAMETERS (q=10, p=20)\n")
 
-    eta = np.zeros(q)
+    attack_detection_rate, num_iterations, estimation_accuracy  = ISTA_with_attacks(p, q, C, tau, lam,x_sparsity,a_sparsity, "UNAWARE", noisy=False)
+    print("| UNAWARE - CLEAN | \n1. The rate of attack detection is: ", attack_detection_rate, " calculated over 20 run\n2. The mean estimation accuracy is: ", estimation_accuracy, "\n")
 
-    print("\SECOND EXERCISE WITH SUGGESTED PARAMETERS (q=10, p=20)\n")
+    attack_detection_rate, num_iterations, estimation_accuracy  = ISTA_with_attacks(p, q, C, tau, lam,x_sparsity,a_sparsity, "AWARE", noisy=False)
+    print("| AWARE - CLEAN | \n1. The rate of attack detection is: ", attack_detection_rate, " calculated over 20 run\n2.  The mean estimation accuracy is: ", estimation_accuracy, "\n")
 
-    print("             QUESTION 2\n- Estimation accuracy: is the estimation of x_tilda accurate?")
-    correct_estimations, num_iterations, estimation_accuracy  = ISTA_20_runs_with_attacks(p, q, C, tau, lam,eta,sparsity,2, "UNAWARE")
-    print(" UNAWARE | The rate of attack detection is: ", correct_estimations, " calculated over 20 run\n The mean estimation accuracy is: ", estimation_accuracy, "\n")
+    attack_detection_rate, num_iterations, estimation_accuracy  = ISTA_with_attacks(p, q, C, tau, lam,x_sparsity,a_sparsity, "UNAWARE", noisy=True)
+    print("| UNAWARE - NOISY | \n1. The rate of attack detection is: ", attack_detection_rate, " calculated over 20 run\n2. The mean estimation accuracy is: ", estimation_accuracy, "\n")
 
-exercise_2()
+    attack_detection_rate, num_iterations, estimation_accuracy  = ISTA_with_attacks(p, q, C, tau, lam,x_sparsity,a_sparsity, "AWARE", noisy=True)
+    print("| AWARE - CLEAN | \n1. The rate of attack detection is: ", attack_detection_rate, " calculated over 20 run\n2. The mean estimation accuracy is: ", estimation_accuracy, "\n")
+
+
+task_1()
+#task_2()
