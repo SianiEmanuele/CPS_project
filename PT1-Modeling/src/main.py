e7eee089c440ad8b00c1ad6f311e1aec832f478d
SianiEmanuele <sianiemanuele@gmail.com>
2024-08-20 01:45:30 +0200

    chore: Add observer function for task 4

--- PT1-Modeling/src/main.py
+++ PT1-Modeling/src/main.py
@@ -97,6 +97,31 @@ def Localization_with_attacks(n, q, G, tau, lam, y):
 
     return w_estimated, w_estimated_supp, iterations
 
+def observer(n, q, A, G, tau, lam, y, K):
+
+    # Estimate x_tilda using ISTA
+    lam_weights = np.concatenate((np.full(n, 10), np.full(q,20)))
+    x_hat = []
+    a_hat = []
+    z_hat = []
+
+    z_0 = np.zeros(n+q)
+    x_hat.append(z_0[:n])
+    a_hat.append(z_0[n:])
+    z_hat.append(z_0)
+
+    for k in range(K-1):
+        z = z_hat[k] + (np.dot(tau, np.dot(G.T, (y[:,k] - np.dot(G, z_hat[k])))))
+        gamma = tau * lam * lam_weights
+        z_hat_plus = IST(z, gamma)
+        x_hat.append(np.dot(A,z_hat_plus[:n]))
+        a_hat.append(z_hat_plus[n:])
+        z_hat.append(np.hstack((x_hat[k+1], a_hat[k+1])))
+    print("X: ", x_hat[-1])
+    print("A: ", a_hat[-1])
+
+    return x_hat, a_hat
+
 #task 1
 def task_1():
 
@@ -444,9 +469,67 @@ def task_3():
 
     plt.show()
 
+def task_4():
+    cwd = os.getcwd()
+    #original matrices
+    mat = sio.loadmat(cwd + r'/src/utils/tracking_moving_targets.mat')
+    A = mat['A']
+    y = mat['Y']
+    D = mat['D']
+    n = D.shape[1]
+    q = D.shape[0]
+    K = y.shape[1]
+    print(K)
 
+    G = np.hstack((D, np.eye(q)))
+    #normalize G
+    G = stats.zscore(G, axis=0)
+
+    tau = 1 / (np.linalg.norm(G, ord=2)**2) - 10**(-8)
+    lam = 1
+    observer(n, q, A, G, tau, lam, y, K)
+    # # Extract the estimated targets' location by taking the 3 greatest values of the first n elements of w_estimated
+    # estimated_targets_location = np.argsort(w_estimated[:n])[-3:]
+
+    # # Extract the estimated attacked vectors from the support of the last q eleemnts of w_estimated
+    # estimated_attacked_sensors = np.where(w_estimated[n:] != 0)[0]
+    
+    # print("Estimated targets location: ", estimated_targets_location)
+    # print("Estimated attacked sensors: ", estimated_attacked_sensors)
+
+    H = 10  # Grid's height (# celle)
+    L = 10  # Grid's length (# celle)
+    W = 100  # Cell's width (cm)
+
+    room_grid = np.zeros((2, n))
+
+    for i in range(n):
+        room_grid[0, i] = W//2 + (i % L) * W
+        room_grid[1, i] = W//2 + (i // L) * W
+
+#     # Plots
+#     plt.figure()
+#     plt.plot(room_grid[0, estimated_targets_location], room_grid[1, estimated_targets_location], 's', markersize=9, 
+#             markeredgecolor=np.array([40, 208, 220])/255, 
+#             markerfacecolor=np.array([40, 208, 220])/255)
+#     plt.grid(True)
+#     plt.legend(['Targets'], loc='best')
+# #     plt.plot(room_grid[0, estimated_attacked_sensors], room_grid[1, estimated_attacked_sensors], 's', markersize=9, 
+# #             markeredgecolor=np.array([255, 0, 0])/255, 
+# #             markerfacecolor=np.array([255, 0, 0])/255)
+
+#     plt.xticks(np.arange(100, 1001, 100))
+#     plt.yticks(np.arange(100, 1001, 100))
+#     plt.xlabel('(cm)')
+#     plt.ylabel('(cm)')
+#     plt.axis([0, 1000, 0, 1000])
+#     plt.gca().set_aspect('equal', adjustable='box')
+
+#     plt.show()
+    return 
 
 if __name__ == "__main__":
     #task_1()
     #task_2()
-    task_3()
+    #task_3()
+    task_4()
